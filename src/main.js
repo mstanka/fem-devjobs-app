a85b720d8d7d2cfd4828842c021947f00f413ee5
mstanka <mice78.mm@gmail.com>
2021-08-14 12:40:42 +0200

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import router from './router.js';
+import store from './store/index.js';
 import App from './App.vue';
 import './style/index.css';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 app.mount('#app');

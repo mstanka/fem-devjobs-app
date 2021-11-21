33e1ff29a999f12c9f28e43e4b7c50a78b0395f9
mstanka <stmmarketa@gmail.com>
2021-11-21 20:13:19 +0100

    add modal for mobile filter

--- src/components/JobFilter.vue
+++ src/components/JobFilter.vue
@@ -41,6 +41,7 @@
           class="
             p-2
             pl-4
+            w-full
             rounded-md
             inline-block
             lg:hidden
@@ -56,7 +57,7 @@
             rounded-md
             hidden
             lg:inline-block
-            w-full
+            w-4/5
             dark:bg-primary-very-dark-blue
             cursor-pointer
           "
@@ -65,21 +66,24 @@
     </div>
 
     <!--Second part-->
-    <div class="flex flex-1 items-center justify-center md:justify-end">
-      <svg
-        class="
-          w-7
-          h-7
-          text-secondary-dark-grey
-          dark:text-secondary-white
-          fill-current
-          md:hidden
-        "
-      >
-        <path
-          d="M19.108 0H.86a.86.86 0 00-.764.455.833.833 0 00.068.884l6.685 9.202.007.01c.242.32.374.708.375 1.107v7.502a.825.825 0 00.248.594.865.865 0 00.942.18l3.756-1.4c.337-.1.56-.41.56-.784v-6.092c0-.399.132-.787.375-1.108l.007-.009 6.685-9.202c.19-.26.217-.6.068-.884A.86.86 0 0019.108 0z"
-        />
-      </svg>
+    <div class="flex flex-1 items-center justify-end">
+      <button @click="openModal">
+        <svg
+          class="
+            w-7
+            h-7
+            text-secondary-dark-grey
+            dark:text-secondary-white
+            fill-current
+            md:hidden
+            cursor-pointer
+          "
+        >
+          <path
+            d="M19.108 0H.86a.86.86 0 00-.764.455.833.833 0 00.068.884l6.685 9.202.007.01c.242.32.374.708.375 1.107v7.502a.825.825 0 00.248.594.865.865 0 00.942.18l3.756-1.4c.337-.1.56-.41.56-.784v-6.092c0-.399.132-.787.375-1.108l.007-.009 6.685-9.202c.19-.26.217-.6.068-.884A.86.86 0 0019.108 0z"
+          />
+        </svg>
+      </button>
       <div
         class="
           hidden
@@ -137,7 +141,16 @@
       </div>
 
       <div class="bg-primary-violet rounded-md py-3 px-3 mr-1">
-        <svg class="w-6 h-6 dark:text-secondary-white fill-current md:hidden">
+        <svg
+          class="
+            w-6
+            h-6
+            dark:text-secondary-white
+            fill-current
+            md:hidden
+            cursor-pointer
+          "
+        >
           <path
             d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z"
           />
@@ -148,12 +161,32 @@
       </div>
     </div>
   </div>
+
+  <!--Modal-->
+  <div>
+    <transition name="modal">
+      <job-filter-modal v-if="showModal" @close="closeModal" />
+    </transition>
+  </div>
 </template>
 
 <script>
+import JobFilterModal from "./JobFilterModal.vue";
+
 export default {
+  components: { JobFilterModal },
   data() {
-    return {};
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
   },
 };
 </script>
---
+++ src/components/JobFilterModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    class="
+      fixed
+      z-20
+      top-0
+      left-0
+      w-full
+      h-full
+      bg-primary-midnight bg-opacity-75
+    "
+  >
+    <div class="relative z-30 w-4/5 mx-auto mt-80">
+      <div
+        class="rounded-lg p-6 bg-secondary-white dark:bg-primary-very-dark-blue"
+      >
+        <div
+          class="
+            border-b border-secondary-grey
+            dark:border-secondary-dark-grey
+            pb-5
+          "
+        >
+          <div class="flex flex-column items-center">
+            <svg class="w-7 h-7 text-primary-violet fill-current">
+              <path
+                d="M14.358 2.451A8.3 8.3 0 008.448 0a8.3 8.3 0 00-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 01-3.051-3.054 3.057 3.057 0 013.05-3.055 3.057 3.057 0 013.052 3.055 3.057 3.057 0 01-3.051 3.054z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Filter by location..."
+              class="p-2 w-full dark:bg-primary-very-dark-blue cursor-pointer"
+            />
+          </div>
+        </div>
+
+        <div class="flex items-center py-6">
+          <input
+            type="checkbox"
+            class="bg-secondary-grey w-4 h-4 p-4 cursor-pointer"
+          />
+          <label
+            for="checkbox"
+            class="
+              text-primary-midnight
+              dark:text-secondary-white
+              font-bold
+              ml-3
+            "
+            >Full Time Only
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          class="
+            font-bold
+            cursor-pointer
+            text-center
+            bg-primary-violet
+            rounded-md
+            py-3
+            px-3
+            w-full
+          "
+          @click="$emit('close')"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+</style>

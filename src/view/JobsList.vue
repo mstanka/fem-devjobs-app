c8e68c7f7bf30bfa9689c8c06e007a732b22aa96
mstanka <stmmarketa@gmail.com>
2021-11-21 14:13:00 +0100

    add jobFilter for mobile

--- src/view/JobsList.vue
+++ src/view/JobsList.vue
@@ -5,19 +5,24 @@
       dark:bg-primary-midnight
       text-secondary-white
       py-10
-      max-w-screen-2xl w-full m-auto
+      max-w-screen-2xl
+      w-full
+      m-auto
     "
   >
+    <job-filter></job-filter>
     <card-job-list :jobs="currentJobs"></card-job-list>
   </div>
 </template>
 
 <script>
-import CardJobList from '../components/CardJobList.vue';
+import CardJobList from "../components/CardJobList.vue";
+import JobFilter from "../components/JobFilter.vue";
 
 export default {
   components: {
     CardJobList,
+    JobFilter,
   },
   computed: {
     currentJobs() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,43 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'primary-violet': '#5964E0',
-        'primary-light-violet': '#939BF4',
-        'primary-very-dark-blue': '#19202D',
-        'primary-midnight': '#121721',
-        'secondary-white': '#fff',
-        'secondary-light-grey': '#f4f6f8',
-        'secondary-grey': '#9daec2',
-        'secondary-dark-grey': '#6e8098',
+        "primary-violet": "#5964E0",
+        "primary-light-violet": "#939BF4",
+        "primary-very-dark-blue": "#19202D",
+        "primary-midnight": "#121721",
+        "secondary-white": "#fff",
+        "secondary-light-grey": "#f4f6f8",
+        "secondary-grey": "#9daec2",
+        "secondary-dark-grey": "#6e8098",
       },
       fontFamily: {
-        kumbh: ['Kumbh Sans', 'sans-serif'],
+        kumbh: ["Kumbh Sans", "sans-serif"],
       },
       fontSize: {
-        h1: ['28px', '34px'],
-        h2: ['24px', '29px'],
-        h3: ['20px', '24px'],
-        h4: ['14px', '18px'],
-        body: ['16px', '26px'],
+        h1: ["28px", "34px"],
+        h2: ["24px", "29px"],
+        h3: ["20px", "24px"],
+        h4: ["14px", "18px"],
+        body: ["16px", "26px"],
       },
     },
   },
   variants: {},
   plugins: [
-    require('tailwindcss-pseudo-elements'),
+    require("tailwindcss-pseudo-elements"),
     plugin(({ addUtilities }) => {
       const newUtilities = {
-        '.empty-content': {
+        ".empty-content": {
           content: "''",
         },
       };
       addUtilities(newUtilities, {
-        variants: ['before', 'after'],
+        variants: ["before", "after"],
       });
     }),
   ],
